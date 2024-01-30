5731dfc3ba62835b6db77c57a24a80147f4f8dfc
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-01-30 11:08:28 +0800

    update ChatboxManager.cs

--- Assets/Scripts/Chatbox.cs
+++ Assets/Scripts/Chatbox.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 用來儲存對話框的訊息
+// ex: 聊天對象的人物頭像, 對話框, ...
 public class Chatbox : MonoBehaviour
 {
     // Start is called before the first frame update
--- Assets/Scripts/ChatboxManager.cs
+++ Assets/Scripts/ChatboxManager.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChatboxManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+// 用來控制Chatbox的生成
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class ChatboxManager : ScriptableObject
+{
+    
 }
