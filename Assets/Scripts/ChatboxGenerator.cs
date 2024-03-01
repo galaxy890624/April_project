3781484f86416a7c8dadca29596a0272d6f31df2
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-03-01 11:41:39 +0800

    Update ..\ScriptableObject

--- Assets/Scripts/ChatboxGenerator.cs
+++ Assets/Scripts/ChatboxGenerator.cs
@@ -1,37 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 用來控制Chatbox的生成
 
 public class ChatboxGenerator : MonoBehaviour
 {
-    public Chatbox chatbox; // 注意：我們將 Chatbox 更改為小寫，以避免與類型名稱混淆
+    public Chatbox Chatbox;
+
+    #region singleton
+    // 可以在其他代碼進行訪問
+    static ChatboxGenerator instance;
+    public ChatboxManager ChatboxManager;
+    public Text ItemInfo;
+
+    // public List<GameObject> Slots = new List<GameObject>();
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        instance = this;
+    }
+    #endregion
 
     // initialize
     private void OnEnable()
     {
-        // 確保 chatbox 已經被實例化
-        if (chatbox == null)
+        // 確保 Chatbox 已經被實例化
+        if (Chatbox == null)
         {
-            // 實例化 chatbox
-            chatbox = ScriptableObject.CreateInstance<Chatbox>();
+            // 實例化 Chatbox
+            Chatbox = ScriptableObject.CreateInstance<Chatbox>();
         }
-
-        // print($"<color=#ff00ff>Info:<color=#00ff00>{chatbox.Info}</color></color>");
-    }
-
-    // Game logic
-    // Update is called once per frame
-    private void Update()
-    {
-
     }
 
     public void 產生對話框()
-    {
-        // 使用 chatbox 來實例化對話框，你需要根據 Chatbox 類別的實際內容進行調整
-        Instantiate(chatbox, Vector3.zero, Quaternion.identity); // **要指定parent object
-        print($"<color=#ff00ff>Info:<color=#00ff00>{chatbox.Info}</color></color>");
+    {        
+        print($"<color=#ff00ff>Info:<color=#00ff00>{Chatbox.Info}</color></color>");
     }
 }
